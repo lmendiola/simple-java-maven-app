a4e206f4120e7a766cbe9155d01f0cacc7d23d14
lmendiola <32039287+lmendiola@users.noreply.github.com>
2020-06-25 10:01:18 +0200

    cambios para probar jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        system.out.println("Fin ejecución");
     }
 
     private final String getMessage() {
